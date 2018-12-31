ce8f25c8ac3f71c68531a5b89ef249ffcc501453
Koshiro Kato <kato2918@gmail.com>
2018-12-31 14:45:08 +0900

    Add a file

---
+++ led.sh
@@ -0,0 +1,38 @@
+echo 0 > /dev/myled0
+sleep 1s
+echo 1 > /dev/myled0
+sleep 1s
+echo 2 > /dev/myled0
+sleep 1s
+echo 3 > /dev/myled0
+sleep 1s
+echo 4 > /dev/myled0
+sleep 1s
+echo 5 > /dev/myled0
+sleep 1s
+echo 0 > /dev/myled0
+echo 2 > /dev/myled0
+sleep 1s
+echo 1 > /dev/myled0
+echo 3 > /dev/myled0
+sleep 1s
+echo 0 > /dev/myled0
+echo 4 > /dev/myled0
+sleep 1s
+echo 1 > /dev/myled0
+echo 5 > /dev/myled0
+sleep 1s
+echo 2 > /dev/myled0
+echo 4 > /dev/myled0
+sleep 1s
+echo 3 > /dev/myled0
+echo 5 > /dev/myled0
+sleep 1s
+echo 0 > /dev/myled0
+echo 2 > /dev/myled0
+echo 4 > /dev/myled0
+sleep 1s
+echo 1 > /dev/myled0
+echo 3 > /dev/myled0
+echo 5 > /dev/myled0
+sleep 1s
